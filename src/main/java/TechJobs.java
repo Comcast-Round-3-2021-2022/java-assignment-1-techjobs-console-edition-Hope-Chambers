93e5118d46b2597ff67d1d3b840c3f716fa31f13
Hope <hope_chambers@comcast.com>
2022-08-11 17:11:44 -0400

    Got print out of jobs with correct spacing, test passed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,16 +112,24 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-       for(HashMap<String, String> job : someJobs){
 
-       }
-        System.out.println(someJobs);
+        for (HashMap<String, String> job : someJobs) {
+
+                String stars= "\n*****";
+                System.out.println(stars);
+            for(Map.Entry<String, String> field : job.entrySet()) {
+
+                System.out.println(field.getKey()+ ": " +field.getValue());
+            }
+                System.out.println("*****");
+
+        }
     }
 }
